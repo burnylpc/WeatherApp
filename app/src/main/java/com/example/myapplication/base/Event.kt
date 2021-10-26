64494d6f427e83a68d9a0ea94f401102821da753
burnylpc <burnylpc@gmail.com>
2021-10-27 01:13:36 +0500

    MVI

---
+++ app/src/main/java/com/example/myapplication/base/Event.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication.base
+
+interface Event
+
+//Интерфейс маркер. Не застовляет ничего делать, он как математическое множество, для дженерика
+//Event - событие
--- app/src/main/java/com/example/myapplication/feature/feature_weather_screen/ui/WeatherScreenViewModel.kt
+++ app/src/main/java/com/example/myapplication/feature/feature_weather_screen/ui/WeatherScreenViewModel.kt
@@ -3,11 +3,14 @@ package com.example.myapplication.feature.feature_weather_screen.ui
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.myapplication.base.BaseViewModel
+import com.example.myapplication.base.Event
 import com.example.myapplication.feature.feature_weather_screen.domain.WeatherInteractor
 import com.example.myapplication.feature.feature_weather_screen.domain.model.WeatherDomainModel
+import com.example.myapplication.feature.feature_weather_screen.domain.model.WeatherWindDomainModel
 import kotlinx.coroutines.launch
 
-class WeatherScreenViewModel(private val weatherInteractor: WeatherInteractor) : ViewModel() {
+class WeatherScreenViewModel(private val weatherInteractor: WeatherInteractor) : BaseViewModel<ViewState>() {
 
     val lifeData : MutableLiveData<WeatherDomainModel> = MutableLiveData()
 
@@ -24,4 +27,20 @@ class WeatherScreenViewModel(private val weatherInteractor: WeatherInteractor) :
 
     }
 
+    override fun initialViewState(): ViewState {
+        return ViewState(WeatherDomainModel(0.0, 0.0, 0.0, WeatherWindDomainModel(0.0, 0.0)), "")
+    }
+
+    override suspend fun reduce(event: Event, previousState: ViewState): ViewState? {
+        when(event) {
+            is DataEvent.WeatherRequest -> {
+                weatherInteractor.getWeather("Moscow")
+            }
+        }
+        return null
+    }
+
 }
+
+//object OnLoadData: DataEvent()
+//data class SuccessWeatherRequest(val weatherDomainModel: WeatherDomainModel) : DataEvent()
